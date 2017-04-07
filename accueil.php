601dd6c27d99d06fd59ca6e1048ee73693e5f74f
Tomore4 <thomas.morel77@sfr.fr>
2017-04-07 15:05:39 +0200

    MAJ accueil.php avec données recues

--- accueil.php
+++ accueil.php
@@ -6,7 +6,7 @@
     </div>
 
     <div class="row">
-            <p class="col-lg-5 col-md-5 col-sm-5 col-xs-5 ">Température </p> <div id="temp"></div>
+            <p class="col-lg-offset-2 col-lg-5 col-md-offset-2 col-md-5 col-sm-offset-2 col-sm-5 col-xs-offset-2 col-xs-5">Température </p> <div id="temp"></div>
             <p class="col-lg-offset-2 col-lg-5 col-md-offset-2 col-md-5 col-sm-offset-2 col-sm-5 col-xs-offset-2 col-xs-5">Humidité</p><div id="humi"></div>
     </div>
 
--- main.js
+++ main.js
@@ -15,42 +15,45 @@ jQuery(document).ready(function ($) {
         console.log(data);
         var temp = data['temperature'];
         var humi = data['humidite'];
-        $("#temp").append(temp);
-        $("#humi").append(humi);
+        $("#temp").append(temp+"°C");
+        $("#humi").append(humi+"%");
     });
 
-        // $('#humidite').append("<table class='table-hover table-bordered table-striped'></table>");
-        // "<caption>Liste des relevés de température</caption>";
-        // ligne = "<thead><tr>";
-        // ligne += "<th class=\"danger\">Temérature</th>";
-        // ligne += "<th class=\"danger\">Humidité</th>";
-        // ligne += "</tr></thead>";
-        // $('#humidite table').append(ligne);
-        // $('#humidite table').append("<tbody>");
-        //
-        // for (i = 0; i < data.length; i++) {
-        //     parrain = data[i];
-        //     ligne = "<tr>";
-        //     ligne += "<td>" + temp + "</td>";
-        //     ligne += "<td>" + humi  + "</td>";
-        //     ligne += "</tr>";
-        //     $('#humidite table').append(ligne);
-        // }
-        //
-        //
-        // $('#humidite table').append("</tbody>");
-        // $('table').tablesorter();
-        //
-        // $('#humidite').append("<hr>");
-        //
-        // for (var key in compteur) {
-        //     var value = compteur[key];
-        //     $('#humidite').append(key + " = " + value + "<br>");
-        // }
+        /*$('#humidite').append("<table class='table-hover table-bordered table-striped'></table>");
+        "<caption>Liste des relevés de température</caption>";
+        ligne = "<thead><tr>";
+        ligne += "<th class=\"danger\">Temérature</th>";
+        ligne += "<th class=\"danger\">Humidité</th>";
+        ligne += "</tr></thead>";
+        $('#humidite table').append(ligne);
+        $('#humidite table').append("<tbody>");
+
+        for (i = 0; i < data.length; i++) {
+            parrain = data[i];
+            ligne = "<tr>";
+            ligne += "<td>" + temp + "</td>";
+            ligne += "<td>" + humi  + "</td>";
+            ligne += "</tr>";
+            $('#humidite table').append(ligne);
+
+
+
+        }
+
+
+        $('#humidite table').append("</tbody>");
+        $('table').tablesorter();
+
+        $('#humidite').append("<hr>");
+
+        for (var key in compteur) {
+            var value = compteur[key];
+            $('#humidite').append(key + " = " + value + "<br>");
+        }
 
 
 
 
 
-    //});
+    });   */
 });
