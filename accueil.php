f8c2899a4cd1ae7e6643882d7e50c7117ef180a7
Tomore4 <thomas.morel77@sfr.fr>
2017-04-07 15:36:15 +0200

    MAJ des CSS

--- accueil.php
+++ accueil.php
@@ -2,12 +2,14 @@
 <div class="Derniersreleves">
 
     <div class="row">
-        <p class="col-lg-5 col-md-5 col-sm-5 col-xs-5">Dernier relevé de température et d'humidté effectué le: </p><div id="dernier"></div>
+        <p class="col-lg-offset-2 col-lg-5 col-md-offset-2 col-md-5 col-sm-offset-2 col-sm-5 col-xs-offset-2 col-xs-5">Dernier relevé de température et d'humidté effectué le : </p><div class="date" id="dernier"></div>
     </div>
 
     <div class="row">
-            <p class="col-lg-offset-2 col-lg-5 col-md-offset-2 col-md-5 col-sm-offset-2 col-sm-5 col-xs-offset-2 col-xs-5">Température </p> <div id="temp"></div>
-            <p class="col-lg-offset-2 col-lg-5 col-md-offset-2 col-md-5 col-sm-offset-2 col-sm-5 col-xs-offset-2 col-xs-5">Humidité</p><div id="humi"></div>
+            <p class="col-lg-offset-2 col-lg-5 col-md-offset-2 col-md-5 col-sm-offset-2 col-sm-5 col-xs-offset-2 col-xs-5">Température </p> <div class="données" id="temp"></div>
+    </div>
+    <div class="row">
+            <p class="col-lg-offset-2 col-lg-5 col-md-offset-2 col-md-5 col-sm-offset-2 col-sm-5 col-xs-offset-2 col-xs-5">Humidité</p><div class="données" id="humi"></div>
     </div>
 
 </div>
